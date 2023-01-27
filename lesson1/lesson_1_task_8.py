065315e59a7f61d27c63d3c4016e7bf4a01600d1
MarinaAlPu <marina.pudovkina13@gmail.com>
2023-01-27 13:26:49 +0500

    ДЗ 1

---
+++ lesson1/lesson_1_task_8.py
@@ -0,0 +1,5 @@
+def number(num):    
+    return print(num*8), print(num*8), print(num*0), print(num*0), print(num*5), print(num*5), \
+            print(num*5), print(num*3), print(num*5), print(num*3), print(num*5) 
+
+number(1)
