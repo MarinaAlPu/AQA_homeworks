20badb816265e6d378f7817cf554238ef2e64047
MarinaAlPu <marina.pudovkina13@gmail.com>
2023-01-28 21:40:39 +0500

    ДЗ 2. Задача 2

---
+++ lesson2/lesson_2_task_2.py
@@ -0,0 +1,33 @@
+year = input("Год: ")
+year = int(year)
+
+def is_year_leap(year):
+    if (year % 4 == 0):
+        x = True
+    else:
+        x = False
+    return x
+
+result = is_year_leap(year)    
+print("год ", year, ":", result)
+
+
+# С ПРОВЕРКОЙ НА ОТРИЦАТЕЛЬНОСТЬ
+# year = input("Год: ")
+# year = int(year)
+
+# def is_year_leap(year):
+#     import math
+#     for year in range(int(-math.inf), 0): # КАК ОБОЗНАЧИТЬ ДИАПАЗОН ОТ -БЕСКОНЕЧНОСТИ ДО 0
+#         print("Введите положительное значение: ")
+#         year = input("Год: ")
+#         year = int(year) 
+#     
+#         if (year % 4 == 0):
+#             x = True
+#         else:
+#             x = False
+#         return x
+
+# result = is_year_leap(year)    
+# print("год ", year, ":", result)    
