3de4527a7f752750639244ebdacbfad46961f0fc
MarinaAlPu <marina.pudovkina13@gmail.com>
2023-01-30 00:03:35 +0500

    Исправления в задаче 7

--- lesson2/lesson_2_task_7.py
+++ lesson2/lesson_2_task_7.py
@@ -1,8 +1,8 @@
 lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
 
-def sum(x):
+def sum(lst):
     result = 0
-    for L in x:
+    for L in lst:
         result = result + L
     print(result)
 
